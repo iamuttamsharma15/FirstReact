64364eb19974edbba0547b07c78b4772afa12cb0
uttamsharma446 <suttam446@gmail.com>
2023-11-30 11:53:55 +0530

    changed name

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
 import React from "react";
- import Details from "./Details";
+import Details from "./Details";
 
-function App(props)
-{
-    return <div className="box">
-    <div className="top-box">
-    <h1 className="heading">{props.name}</h1>
+function App(props) {
+  return (
+    <div className="box">
+      <div className="top-box">
+        <h1 className="heading">{props.name}</h1>
+      </div>
+
+      <div className="inside-box">
+        <img className="img-box" src={props.img} alt="" />
+        <Details detailInfo={props.tel} />
+        <Details detailInfo={props.email} />
+      </div>
     </div>
-   
-    <div className="inside-box">
-
-       
-       
-       <img className="img-box"src={props.img} alt=""/>
-        <Details
-            detailInfo={props.tel}
-        />
-        <Details
-            detailInfo={props.email}
-        />
-       
-  
-
-
-    </div>
-
-    </div> ;
+  );
 }
 export default App;
--- src/Contacts.jsx
+++ src/Contacts.jsx
@@ -1,26 +1,24 @@
-
-const Contacts=[
-  
-    {
-      id:1,
-       name:"Uttam Sharma",
-      img:"img.jpg",
-      tel:"+852462524",
-      email:"suttam446@gmail.com"
-    },
-    { 
-      id:2,
-      name:"Akshay Kumar",
-      img:"img3.jpg",
-      tel:"7080246071",
-      email:"yess@gmail.com"
-    },
-    {
-      id:3,
-      name:"Jack Bauer",
-      img:"img1.jpg",
-      tel:"+2545625412",
-      email:"jackbauer@gmail.com"
-    }
+const Contacts = [
+  {
+    id: 1,
+    name: "Uttmm Sharma",
+    img: "img.jpg",
+    tel: "+852462524",
+    email: "suttam446@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Akshay Kumar",
+    img: "img3.jpg",
+    tel: "7080246071",
+    email: "yess@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Jack Bauer",
+    img: "img1.jpg",
+    tel: "+2545625412",
+    email: "jackbauer@gmail.com",
+  },
 ];
 export default Contacts;
